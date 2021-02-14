324f69541c2f54c8303b712327fc0d351fa1edf7
Gustavo Gonzalez <gusgon02@hotmail.com>
2021-02-14 20:41:21 -0300

    modificando o site para react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import './data';
 
 function App() {
+
+  const openMenu = () =>{
+    document.querySelector(".sidebar").classList.add("open");
+  }
+
+  const closeMenu = () =>{
+    document.querySelector(".sidebar").classList.remove("open");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+
+      <header className="header">
+            <div className="brand">
+                <button onClick={openMenu}>
+                    &#9776;
+                </button>
+                <a href="index.html">GusGames</a>
+            </div>
+            <div className="header-links">
+                <a href="cart.html">Carrinho</a>
+                <a href="login.html">Login ou Cadastre-se</a>
+            </div>
       </header>
+
+      <aside className="sidebar">
+        <h3>Categorías dos jogos</h3>
+        <button className="sidebar-close-button" onClick={closeMenu}>
+          x
+        </button>
+        <ul>
+          <li>
+            <a href="index.html">Ação</a>
+          </li>
+          <li>
+            <a href="index.html">Aventura</a>
+          </li>
+          <li>
+            <a href="index.html">Esporte</a>
+          </li>
+        </ul>
+      </aside>
+        
+      <main className="main">
+        <div className="content">
+          <ul className="products">
+            {
+              products.map(product =>
+                <li>
+                  <div className="product">   
+                    <img className="product-image" src={product.image} alt="product"/>
+                    <div className="product-name">
+                      <a href="product.html">{product.name}</a>
+                    </div>
+                    <div className="product-pricing">
+                      {product.pricing}
+                    </div>
+                    <div className="product-score">
+                      {product.score}
+                    </div>
+                  </div>
+                </li>
+              )
+            }
+          </ul>
+        </div>   
+      </main>
+
+      <footer className="footer">
+        Todos os direitos reservados.
+      </footer>
+
     </div>
   );
 }
